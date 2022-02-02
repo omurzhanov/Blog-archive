e86aded283c9e42bf956ec5e88caffbb5e189338
Zalkar <omurzhanovz@gmail.com>
2022-02-02 20:59:22 +0100

    commit

---
+++ blog/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'slug', 'author', 'publish', 'status',
+    )
+    list_filter = (
+        'status', 'created', 'publish', 'author',
+    )
+    inlines = [
+        CommentInline,
+    ]
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',) 
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zalkar/Desktop/DJango3ByExamples/first/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
